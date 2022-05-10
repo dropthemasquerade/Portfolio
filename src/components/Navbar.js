6537a23ace5917f2ffc013f393c80c589023e496
land <land@landpacks-MacBook-Pro.local>
2022-05-10 23:38:20 +0800

    rm resume & blog from nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -82,25 +82,25 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/resume"
-                onClick={() => updateExpanded(false)}
-              >
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-              </Nav.Link>
-            </Nav.Item>
+            // <Nav.Item>
+            //   <Nav.Link
+            //     as={Link}
+            //     to="/resume"
+            //     onClick={() => updateExpanded(false)}
+            //   >
+            //     <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
+            //   </Nav.Link>
+            // </Nav.Item>
 
-            <Nav.Item>
-              <Nav.Link
-                href="https://blogs.soumya-jit.tech/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <ImBlog style={{ marginBottom: "2px" }} /> Blogs
-              </Nav.Link>
-            </Nav.Item>
+            // <Nav.Item>
+              // <Nav.Link
+              //   href="https://blogs.soumya-jit.tech/"
+              //   target="_blank"
+              //   rel="noreferrer"
+              // >
+                // <ImBlog style={{ marginBottom: "2px" }} /> Blogs
+              // </Nav.Link>
+            // </Nav.Item>
           </Nav>
         </Navbar.Collapse>
       </Container>
